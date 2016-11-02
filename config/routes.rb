f153513c21dd651b01c6bf4188ad512ac830f6c2
kelishrestha <kylesth@gmail.com>
2016-11-02 21:48:21 +0545

    Define routes for getting status for app.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      get '/status', to: 'base#status'
+    end
+  end
 end
